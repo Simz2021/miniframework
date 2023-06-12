364a5427725e9b19ec84f948f74afec9f668c1ea
SimsBiz101 <sprojects2021@outlook.com>
2023-06-12 21:00:16 +0200

    first commit

---
+++ models/ItemModel.php
@@ -0,0 +1,43 @@
+<?php
+
+class ItemModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        // Connect to the database
+        $dsn = 'mysql:host=localhost;dbname=your_database_name';
+        $username = 'your_username';
+        $password = 'your_password';
+        $this->db = new PDO($dsn, $username, $password);
+    }
+
+    public function addItem($item)
+    {
+        $sql = 'INSERT INTO items (name) VALUES (?)';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$item]);
+    }
+
+    public function deleteItem($itemId)
+    {
+        $sql = 'DELETE FROM items WHERE id = ?';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$itemId]);
+    }
+
+    public function editItem($itemId, $newItemName)
+    {
+        $sql = 'UPDATE items SET name = ? WHERE id = ?';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$newItemName, $itemId]);
+    }
+
+    public function getItems()
+    {
+        $sql = 'SELECT * FROM items';
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+}
